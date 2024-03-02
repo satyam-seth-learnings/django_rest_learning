26ded6cdb621ede90bac3fbf7aa45a3a3d646272
satyam-seth <satyam1998.1998@gmail.com>
2024-03-02 12:44:22 +0530

    add login api

--- Geekyshows/5.Complete Authentication API with JWT in Django REST Framework/djangoauthapi1/account/views.py
+++ Geekyshows/5.Complete Authentication API with JWT in Django REST Framework/djangoauthapi1/account/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from account.serializers import UserRegistrationSerializer
+from account.serializers import UserRegistrationSerializer, UserLoginSerializer
 
 class UserRegistrationView(APIView):
     def post(self, request, format=None):
@@ -9,4 +10,16 @@ class UserRegistrationView(APIView):
         if serializer.is_valid(raise_exception=True):
             user = serializer.save()
             return Response({'msg': 'Registration Success'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserLoginView(APIView):
+    def post(self, request, format=None):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            email = serializer.data.get('email')
+            password = serializer.data.get('password')
+            user = authenticate(email=email, password=password)
+            if user is not None:
+                return Response({'msg':'Login Success'}, status=status.HTTP_200_OK)
+            return Response({'errors':{'non_field_errors':['Email or Password is not Valid']}}, status=status.HTTP_404_NOT_FOUND)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
