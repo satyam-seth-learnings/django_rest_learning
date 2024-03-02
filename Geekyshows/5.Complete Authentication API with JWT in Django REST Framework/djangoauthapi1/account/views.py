43f724b66115091279e8bc1f6a689eb88537f6a0
satyam-seth <satyam1998.1998@gmail.com>
2024-03-02 13:56:21 +0530

    generate jwt token for user

--- Geekyshows/5.Complete Authentication API with JWT in Django REST Framework/djangoauthapi1/account/views.py
+++ Geekyshows/5.Complete Authentication API with JWT in Django REST Framework/djangoauthapi1/account/views.py
@@ -4,6 +4,17 @@ from rest_framework import status
 from rest_framework.views import APIView
 from account.serializers import UserRegistrationSerializer, UserLoginSerializer
 from account.renderers import UserRenderer
+from rest_framework_simplejwt.tokens import RefreshToken
+
+# Generate token manually
+# https://django-rest-framework-simplejwt.readthedocs.io/en/latest/creating_tokens_manually.html#creating-tokens-manually
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
 
 class UserRegistrationView(APIView):
     renderer_classes = [UserRenderer]
@@ -12,7 +23,8 @@ class UserRegistrationView(APIView):
         serializer = UserRegistrationSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             user = serializer.save()
-            return Response({'msg': 'Registration Success'}, status=status.HTTP_201_CREATED)
+            token = get_tokens_for_user(user)
+            return Response({'token': token, 'msg': 'Registration Success'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class UserLoginView(APIView):
