b37aa8d4d55faf479dd71ed666c2e2d8dbb70c0d
satyam-seth <satyam1998.1998@gmail.com>
2024-03-02 14:32:56 +0530

    add chenage password api

--- Geekyshows/5.Complete Authentication API with JWT in Django REST Framework/djangoauthapi1/account/views.py
+++ Geekyshows/5.Complete Authentication API with JWT in Django REST Framework/djangoauthapi1/account/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from account.serializers import UserRegistrationSerializer, UserLoginSerializer, UserProfileSerializer
+from account.serializers import UserRegistrationSerializer, UserLoginSerializer, UserProfileSerializer, UserChangePasswordSerializer
 from account.renderers import UserRenderer
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated
@@ -50,3 +50,10 @@ class UserProfileView(APIView):
         serailizer = UserProfileSerializer(request.user)
         return Response(serailizer.data, status=status.HTTP_200_OK)
         
+class UserChangePasswordView(APIView):
+    renderer_classes = [UserRenderer]
+    permission_classes = [IsAuthenticated]
+    def post(self, request, format=None):
+        serializer = UserChangePasswordSerializer(data=request.data, context={'user':request.user})
+        serializer.is_valid(raise_exception=True)
+        return Response({'msg':'Password Changed Successfully'}, status=status.HTTP_200_OK)
